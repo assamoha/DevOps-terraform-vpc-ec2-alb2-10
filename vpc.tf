7d818eaae06568962d58fdb5960c7ac7b1a37f75
assamoha <assamohalinux@gmail.com>
2024-08-30 19:29:01 -0400

    modified vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vpc1" {
   cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
   tags = {
-    Name = "terraform-vpc"
+    Name = "Terraform-vpc"
     Env  = "Dev"
     Team = "DevOps"
   }
