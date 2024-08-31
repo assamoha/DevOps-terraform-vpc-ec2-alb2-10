07fe6a2f5ea9621185b8f4d1f8c2ba131780b0ac
assamoha <assamohalinux@gmail.com>
2024-08-31 16:09:00 -0400

    backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "week10-asm-terraform"
-    key            = "week10/terraform.tfstate"
+    key            = "week102/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "State-Log"
