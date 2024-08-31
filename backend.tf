19b28dc5ad2cad536e3c0957eb0d295147c3eecd
assamoha <assamohalinux@gmail.com>
2024-08-31 16:05:05 -0400

    added-jenkinsfile

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "week10-asm-terraform"
-    key            = "week102/terraform.tfstate"
+    key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "State-Log"
